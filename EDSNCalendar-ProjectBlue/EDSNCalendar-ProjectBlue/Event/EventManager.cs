2fba42007d508512eca31c80537eb7824a2211f8
PerrettaAS <scottaperretta@gmail.com>
2016-11-05 17:32:25 -0400

    Add skeleton for EventManager class.

---
+++ EDSNCalendar-ProjectBlue/EDSNCalendar-ProjectBlue/Event/EventManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EDSNCalendar_ProjectBlue.Event
+{
+    /// <summary>
+    /// The EventManager is a static class that handles all functionality 
+    /// of events that exist in the calendar system.
+    /// </summary>
+    public static class EventManager
+    {
+
+        /// <summary>
+        /// Collection of events that are ready to be reviewed by an administrator.
+        /// </summary>
+        private static List<Event> submittedEvents = new List<Event>();
+
+        public static List<Event> SubmittedEvents
+        {
+            get
+            {
+                return submittedEvents;
+            }
+        }
+
+        /// <summary>
+        /// Collection of events that are ready to be reviewed by an administrator.
+        /// </summary>
+        private static List<Event> publishedEvents = new List<Event>();
+
+        public static List<Event> PublishedEvents
+        {
+            get
+            {
+                return publishedEvents;
+            }
+        }
+
+        static EventManager()
+        {
+            /// TODO: Query through the database and populate all submitted/published events to their appropriate collection.
+        }
+
+    }
+}
