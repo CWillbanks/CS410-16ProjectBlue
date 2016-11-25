bf6bdeccdbc1409b39cc402116b0224985a98b55
algburton <algburton@gmail.com>
2016-11-25 12:03:32 -0500

    Add end date to sql schema

--- documents/CreateSchema.sql
+++ documents/CreateSchema.sql
@@ -30,6 +30,7 @@ CREATE TABLE calendarevent
 	iEventId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     vEventTitle VARCHAR(255),
     dEventDate VARCHAR(50),
+    dEndDate VARCHAR(50),
     vStartTime VARCHAR(50),
     vEndTime VARCHAR(50),
     bAllDay BIT,
@@ -69,23 +70,23 @@ VALUES('Categories'),('Tags'),('Region'),('Country'),('Programs');
 INSERT INTO property(iPropertyTypeId,vProperty)
 VALUES(1, 'Movies'),(1,'Religion'),(2,'#Fitness'),(2,'#Community'),(3, 'Hartford, Ct'),(4,'USA'),(4,'Canada'),(5,'Birthday');
 
-INSERT INTO calendarevent(vEventTitle, dEventDate, vStartTime, vEndTime, bAllDay, vVenueName, vAddress, vDescription, vOrganizerName,
+INSERT INTO calendarevent(vEventTitle, dEventDate, dEndDate, vStartTime, vEndTime, bAllDay, vVenueName, vAddress, vDescription, vOrganizerName,
 						   vOrganizerEmail, vOrganizerPhoneNumber, vOrganizerURL, vCost, vRegistrationURL, vSubmitterName, vSubmitterEmail, dtPublishDate ,bPublished, bActive)
-VALUES('Submitted Event1(StartFinish)(Active)', '2016-11-8', '2016-11-8T13:00:00', '2016-11-8T15:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+VALUES('Submitted Event1(StartFinish)(Active)', '2016-11-8', '2016-11-8', '2016-11-8T13:00:00', '2016-11-8T15:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person1', 'AdamEmail@email.com', NULL,0, 1),
-	  ('Submitted Event2(AllDay)(Active)', '2016-11-27', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+	  ('Submitted Event2(AllDay)(Active)', '2016-11-27', '2016-11-27', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person2', 'AdamEmail@email.com', NULL,0, 1),
-	  ('Submitted Event3(StartFinish)(Inactive)', '2016-11-20', '2016-11-20T8:00:00', '2016-11-20T11:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+	  ('Submitted Event3(StartFinish)(Inactive)', '2016-11-20', '2016-11-20', '2016-11-20T8:00:00', '2016-11-20T11:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person3', 'AdamEmail@email.com', NULL,0, 0),
-	  ('Submitted Event4(AllDay)(Inactive)', '2016-11-15', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+	  ('Submitted Event4(AllDay)(Inactive)', '2016-11-15', '2016-11-15', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person4', 'AdamEmail@email.com', NULL,0, 0),
-	  ('Published Event1(StartFinish)(Active)', '2016-11-4', '2016-11-4T12:00:00', '2016-11-4T16:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+	  ('Published Event1(StartFinish)(Active)', '2016-11-4', '2016-11-4', '2016-11-4T12:00:00', '2016-11-4T16:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person5', 'AdamEmail@email.com', '2016-11-30',1, 1),
-	  ('Published Event2(AllDay)(Active)', '2016-11-24', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+	  ('Published Event2(AllDay)(Active)', '2016-11-24', '2016-11-24', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person6', 'AdamEmail@email.com', '2016-11-30',1, 1),
-	  ('Published Event3(StartFinish)(Inactive)', '2016-11-30', '2016-11-30T20:00:00', '2016-11-30T23:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+	  ('Published Event3(StartFinish)(Inactive)', '2016-11-30', '2016-11-30', '2016-11-30T20:00:00', '2016-11-30T23:00:00', 0, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person7', 'AdamEmail@email.com', '2016-11-30',1, 0),
-	  ('Published Event4(AllDay)(Inactive)', '2016-11-1', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
+	  ('Published Event4(AllDay)(Inactive)', '2016-11-1', '2016-11-1', NULL, NULL, 1, 'Nowhere!', 'Nowhere St, Nowhere, 06062', 'This is my description here. Wow. Much description!', 'Adam',
 	   'FakeEmail@email.com', '999-999-9999', NULL, 'FREE', NULL, 'Person8', 'AdamEmail@email.com', '2016-11-30',1, 0);
        
 
